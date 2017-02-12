9385aa72f6d540b3aae7aa7122156c3b282e8fd3
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-02-12 12:37:29 -0500

    update handleEdit logic for save of editable state values, add refs

--- app/assets/javascripts/components/_item.js.jsx
+++ app/assets/javascripts/components/_item.js.jsx
@@ -5,12 +5,17 @@ var Item = React.createClass({
     },
 
   handleEdit() {
+      if(this.state.editable) {
+        var name = this.refs.name.value;
+        var description = this.refs.description.value;
+        console.log('in handleEdit', this.state.editable, name, description);
+      }
       this.setState({editable: !this.state.editable})
     },
 
   render() {
-      var name = this.state.editable ? <input type='text' defaultValue={this.props.item.name} /> : <h3>{this.props.item.name} {this.props.item.id}</h3>
-      var description = this.state.editable ? <input type='text' defaultValue={this.props.item.description} /> : <h3>{this.props.item.description}</h3>
+      var name = this.state.editable ? <input type='text' ref='name' defaultValue={this.props.item.name} /> : <h3>{this.props.item.name} {this.props.item.id}</h3>
+      var description = this.state.editable ? <input type='text' ref='description' defaultValue={this.props.item.description} /> : <h3>{this.props.item.description}</h3>
       return (
         <div>
           {name}
